5c591ec264bb3f77c70a43f69d6d4d2641894ef9
Élite Digitale <vcreation215@gmail.com>
2026-05-13 07:37:37 +0000

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", paddingTop: 50 }}>
+      <h1>Bienvenue sur MonBus CI 🚍</h1>
+      <p>L'application qui te montre ton bus en temps réel.</p>
+    </main>
+  );
+}
